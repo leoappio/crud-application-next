44f64dc00a10792bce239cf2be82bf32a22cadd4
Leonardo <leonardo.appio@gmail.com>
2021-09-28 10:38:42 -0300

    construida tabela e adicionado os icones

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/cliente'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana',34, '1'),
+    new Cliente('João',45, '2'),
+    new Cliente('Pedro',67, '3'),
+    new Cliente('Carlos',25, '4'),
+  ]
+
+
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -11,7 +23,9 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo = "Cadastro">
-        <span> Conteudo </span>
+        <Tabela clientes = {clientes} >
+
+        </Tabela>
       </Layout>
     </div>
   )
