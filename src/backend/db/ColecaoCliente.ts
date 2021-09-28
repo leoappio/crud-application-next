acb13119468aec21980af003d277cbfc86163426
Leonardo <leonardo.appio@gmail.com>
2021-09-28 14:09:12 -0300

    criado repositorio e colecao de clientes

---
+++ src/backend/db/ColecaoCliente.ts
@@ -0,0 +1,33 @@
+import Cliente from "../../core/cliente";
+import ClienteRepositorio from "../../core/ClienteRepositorio";
+
+export default class ColecaoCliente implements ClienteRepositorio{
+
+    conversor = {
+        toFirestore(cliente : Cliente){
+            return{
+                nome: cliente.nome,
+                idade: cliente.idade
+            }
+        },
+        fromFirestore(snapshot, options) : Cliente{
+            const dados = snapshot.data(options)
+            return new Cliente(dados.nome, dados.idade, snapshot.id)
+
+        }
+    }
+    async salvar(cliente: Cliente): Promise <Cliente>{
+        return null
+
+    }
+
+    async excluir(cliente: Cliente): Promise <void>{
+        return null
+
+    }
+
+    async obterTodos(): Promise <Cliente[]>{
+        return null
+
+    }
+}
